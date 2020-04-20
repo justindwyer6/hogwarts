27defc9ecbd9a43e908e0050dc6feebef5a53834
Justin Dwyer <justindwyer6@gmail.com>
2020-04-20 09:51:35 -0700

    apparently tailwind config works in production but not dev??

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   theme: {
     extend: {},
-    backgroundColor: theme => ({
-      'trans-black': 'rgba(0, 0, 0, 0.5)'
-    })
+    // backgroundColor: theme => ({
+    //   'trans-black': 'rgba(0, 0, 0, 0.5)'
+    // })
   },
   variants: {},
   plugins: [],
